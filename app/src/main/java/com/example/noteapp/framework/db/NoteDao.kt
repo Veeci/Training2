8109b114828af34caad94c04c305c6dd8f959f0c
Veeci <doanvietquang15102003@gmail.com>
2024-07-13 16:48:47 +0700

    added dao

--- app/src/main/java/com/example/noteapp/framework/db/NoteDao.kt
+++ app/src/main/java/com/example/noteapp/framework/db/NoteDao.kt
@@ -1,7 +1,23 @@
 package com.example.noteapp.framework.db
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
-interface NoteDao {
+interface NoteDao
+{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addNoteEntity(noteEntity: NoteEntity)
+
+    @Query("SELECT * FROM note WHERE id = :id")
+    suspend fun getNoteEntity(id: Long): NoteEntity?
+
+    @Query("SELECT * FROM note")
+    suspend fun getAllNoteEntities(): List<NoteEntity>
+
+    @Delete
+    suspend fun removeNoteEntity(noteEntity: NoteEntity)
 }
